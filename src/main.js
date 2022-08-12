48f41c7e64d38519c35b2c4e12f5e193438c388f
TimeCarol <l17679360127@gmail.com>
2022-08-13 01:14:26 +0800

    添加进度条

--- src/main.js
+++ src/main.js
@@ -6,15 +6,25 @@ import './plugins/elementui.js'
 import './assets/css/global.css'
 //导入axios
 import axios from 'axios'
+//导入nprogress
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 //配置请求的根路径
 axios.defaults.baseURL = 'http://localhost:8888/api/private/v1/'
+//在request拦截中展示进度条
 //axios请求拦截
 axios.interceptors.request.use(config => {
+  NProgress.start()
   //为请求头添加Token验证的Authorization字段
   const token = sessionStorage.getItem('token')
   if (token) {
     config.headers.Authorization = token
   }
+  return config
+})
+//在response拦截器中隐藏进度条
+axios.interceptors.response.use(config => {
+  NProgress.done()
   return config
 })
 Vue.prototype.$http = axios
